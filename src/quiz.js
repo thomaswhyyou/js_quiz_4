7285daa23fc41959e1037d7ca4b2f975c8803035
ThomasWhyyou <thomas.whyyou@ymail.com>
2013-08-30 09:54:05 -0400

    js quiz 4

--- src/quiz.js
+++ src/quiz.js
@@ -6,3 +6,21 @@
 //      |__|     |__|  |__| |__| |_______/       |__| |_______/       /__/     \__\     \_____\_____\\______/  |__|  /________(__)
 
 var QUIZ_TOPICS = ['javascript', 'jasmine', 'testing'];
+
+
+Array.prototype.sort = function() {
+  var temp_array = [];
+  var target_index;
+  var target_number;
+  var max_i = this.length;
+  for (var i = 0; i < max_i; i++ ) {
+    target_index = this.indexOf(Math.min.apply(null, this));
+    target_number_array = this.splice(target_index, 1);
+    temp_array.push(target_number_array);
+  }
+  // debugger;
+  sorted_array = [].concat.apply([], temp_array);
+  return sorted_array;
+};
+
+
